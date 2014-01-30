9f97ef51c494e8068d5d83aa2eb607b0214c92b3
Armadill0 <armadillo@penguinfriends.org>
2014-01-30 10:08:22 +0100

    added more initial files

---
+++ harbour-tasklist.pro
@@ -0,0 +1,27 @@
+# The name of your app.
+# NOTICE: name defined in TARGET has a corresponding QML filename.
+#         If name defined in TARGET is changed, following needs to be
+#         done to match new name:
+#         - corresponding QML filename must be changed
+#         - desktop icon filename must be changed
+#         - desktop filename must be changed
+#         - icon definition filename in desktop file must be changed
+TARGET = harbour-tasklist
+
+CONFIG += sailfishapp
+
+SOURCES += src/harbour-tasklist.cpp
+
+OTHER_FILES += qml/harbour-tasklist.qml \
+    qml/cover/CoverPage.qml \
+    rpm/harbour-tasklist.spec \
+    rpm/harbour-tasklist.yaml \
+    harbour-tasklist.desktop \
+    TODO.txt \
+    qml/localdb.js \
+    BUGS.txt \
+    qml/pages/AboutPage.qml \
+    qml/pages/EditPage.qml \
+    qml/pages/TaskPage.qml \
+    qml/pages/ListPage.qml
+
---
+++ src/harbour-tasklist.cpp
@@ -0,0 +1,40 @@
+/*
+    TaskList - A small but mighty program to manage your daily tasks.
+    Copyright (C) 2014 Thomas Amler
+    Contact: Thomas Amler <armadillo@penguinfriends.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef QT_QML_DEBUG
+#include <QtQuick>
+#endif
+
+#include <sailfishapp.h>
+
+
+int main(int argc, char *argv[])
+{
+    // SailfishApp::main() will display "qml/template.qml", if you need more
+    // control over initialization, you can use:
+    //
+    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
+    //   - SailfishApp::createView() to get a new QQuickView * instance
+    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
+    //
+    // To display the view, call "show()" (will show fullscreen on device).
+
+    return SailfishApp::main(argc, argv);
+}
+
