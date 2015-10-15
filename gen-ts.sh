d61697d9f5078d72a9a79862f7c44c46201bcb4b
Armadill0 <armadillo@penguinfriends.org>
2015-10-15 07:15:15 +0200

    generator only pdates en_US file; source language updated

--- gen-ts.sh
+++ gen-ts.sh
@@ -7,4 +7,5 @@ TS_SOURCES=localization-sources
 echo "Running TS generator for app '${APP_NAME}'"
 
 echo "Updating ts files..."
-lupdate ${APP_NAME}.pro -ts ${TS_SOURCES}/*.ts
+#lupdate ${APP_NAME}.pro -ts ${TS_SOURCES}/*.ts
+lupdate ${APP_NAME}.pro -ts ${TS_SOURCES}/${APP_NAME}_en_US.ts
